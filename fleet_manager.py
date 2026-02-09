c9cbb58fc17636131a3f2d4542771b64ffb8087f
Lis <lp598@kent.ac.uk>
2026-02-09 19:41:00 +0000

    frame made added if statements

--- fleet_manager.py
+++ fleet_manager.py
@@ -1,22 +1,55 @@
 def main():
 
 
-def 1():
+    def 1():
 
-def 2():
+    def 2():
 
-def 3():
+    def 3():
 
-def 4():
+    def 4():
 
-def 5():
+    def 5():
 
-def 6():
+    def 6():
 
-def 7():
+    def 7():
 
-def 8():
+    def 8():
 
-def 9():
+    def 9():
 
-def 10():
+    def 10():
+
+
+
+
+if  == 1:
+    1()
+
+elif  == 2:
+    2()
+
+elif  == 3:
+    3()
+
+elif == 4:
+    4()
+
+elif == 5:
+    5()
+
+elif == 6:
+    6()
+
+elif == 7:
+    7()
+
+elif == 8:
+    8()
+
+elif == 9:
+    9()
+
+elif ==10:
+    10()
